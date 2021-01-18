7390828cd75a858b4b101db6ef994c625022f5b2
elioenaiferrari <elioenaiferrari@gmail.com>
2021-01-18 09:16:58 -0300

    all integrations

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 
 const { URL, USERNAME, PASSWORD, COMMENTS, FACEBOOK } = process.env;
 
-const SLEEP_TIME = 3000;
+const SLEEP_TIME = 5000;
 const INSTAGRAM_LIMIT_TIME = 60000;
 const TYPE_PARAMS = { delay: 100 };
 
